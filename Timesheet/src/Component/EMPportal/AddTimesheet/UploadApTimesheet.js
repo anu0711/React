49b15356fc4c515112bd77b9932e6a6369fd2136
Naga0712 <109150564+Naga0712@users.noreply.github.com>
2022-09-20 14:14:33 +0530

    img pop

--- Timesheet/src/Component/EMPportal/AddTimesheet/UploadApTimesheet.js
+++ Timesheet/src/Component/EMPportal/AddTimesheet/UploadApTimesheet.js
@@ -51,7 +51,10 @@ class UploadApTimesheet extends Component {
                 console.log(res);
                 setMessage(res.status, res.data);
             })
-
+            const timout1 = setTimeout(() => {
+                window.location.reload(false);
+            }, 1150);
+            return () => clearTimeout(timout1);    
     }
 
     render() {
