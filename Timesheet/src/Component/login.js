448c591774ae5a3d701dfbd3a15d056f0a1b5c18
Naga0712 <109150564+Naga0712@users.noreply.github.com>
2022-09-28 10:04:09 +0530

    logchn

--- Timesheet/src/Component/login.js
+++ Timesheet/src/Component/login.js
@@ -83,14 +83,14 @@ const Login = () => {
     return (
 
         <>
-            <Card className='login' >
-                <div> <h2 style={{ color: "white", fontSize: 35, marginLeft: 520, marginTop: 50, fontFamily: "LucidaConsole" }}>  TIMESHEET AUTOMATION</h2></div>
-                <div style={{ width: 480, marginLeft: 490, padding: 10, backgroundColor: "0,0,0,0.1", boxSizing: 'border-box', borderRadius: 15, boxShadow: "0 0 40px rgba(0,0,0,16)", marginTop: 50, position: "" }}>
+            <Card className='login'  style={{position:'fixed'}}>
+                <div> <h2 style={{ color: "white", fontSize: 35, marginLeft: "48%", marginTop: 50, fontFamily: "LucidaConsole" }}>  TIMESHEET AUTOMATION</h2></div>
+                <div style={{position:"fixed", width: 480, marginLeft: 380, padding: 10, backgroundColor: "0,0,0,0.1", boxSizing: 'border-box', borderRadius: 15, boxShadow: "0 0 40px rgba(0,0,0,16)", marginTop: 0, position: "" }}>
 
                     {/* <Button type="lightdark" style={{ width: 200, height: 50, marginLeft: 100 }}><Link to="dashboard"><b>HR Portal</b></Link></Button><br /><br />
                     <Button type="lightdark" style={{ width: 200, height: 50, marginLeft: 100 }}><Link to="/Siders"><b>Employee Portal</b></Link></Button> */}
                     <h1 style={{ marginLeft: 10, fontSize: 25, color: "white", fontFamily: "LucidaConsole" }}><b>LOGIN PAGE</b></h1>
-                    <Form
+                    <Form 
                         {...formItemLayout}
                         onFinish={onFinish}
                         form={AddProjectForm}
@@ -117,9 +117,9 @@ const Login = () => {
                         ]}>
                             <Input.Password type="password" style={{ width: 450, marginLeft: 5 }} />
                         </Form.Item><br />
-                        <p style={{ color: "white", marginLeft: 110 }}>Change password?<span style={{ marginLeft: 5, }}><Link to="/resetPassword"><b><u>click here..</u></b></Link></span></p><br />
+                        <p style={{ color: "white", marginLeft: 110 }}>Change password?<span style={{ marginLeft: 5, }}><Link to="/resetPassword"><b><u>click here..</u></b></Link></span></p>
                         {/* <Link>click here..</Link> */}
-                        <p style={{ color: "white", marginLeft: 110 }}>Forgot password?<span style={{ marginLeft: 5, }}><Link to="/forgetpassword"><b><u>click here..</u></b></Link></span></p>
+                        <p style={{ color: "white", marginLeft: 110,marginBottom:'10%' }}>Forgot password?<span style={{ marginLeft: 5,marginBottom:'-3%' }}><Link to="/forgetpassword"><b><u>click here..</u></b></Link></span></p>
 
                         <Form.Item>
                             <Space>
